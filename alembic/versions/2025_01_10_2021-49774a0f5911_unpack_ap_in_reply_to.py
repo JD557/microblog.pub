61a266cbb4a9e2c8c985a0d632f3159fa12c96b8
João Costa <jdpc557@gmail.com>
2026-05-24 08:39:44 +0000

    Fix SQL types

--- alembic/versions/2025_01_10_2021-49774a0f5911_unpack_ap_in_reply_to.py
+++ alembic/versions/2025_01_10_2021-49774a0f5911_unpack_ap_in_reply_to.py
@@ -18,11 +18,11 @@ depends_on = None
 
 def upgrade() -> None:
     with op.batch_alter_table('inbox', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('in_reply_to', sa.Integer()))
+        batch_op.add_column(sa.Column('in_reply_to', sa.String(), nullable=True))
         batch_op.create_index(batch_op.f('ix_inbox_in_reply_to'), ['in_reply_to'], unique=False)
 
     with op.batch_alter_table('outbox', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('in_reply_to', sa.Integer()))
+        batch_op.add_column(sa.Column('in_reply_to', sa.String(), nullable=True))
         batch_op.create_index(batch_op.f('ix_outbox_in_reply_to'), ['in_reply_to'], unique=False)
 
     op.execute("UPDATE inbox SET in_reply_to = json_extract(ap_object, '$.inReplyTo')")
